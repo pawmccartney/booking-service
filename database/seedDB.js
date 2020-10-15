7e637fc3abfe30b295d469ca6474018aa1099945
charles Eddy <16qeddy@gmail.com>
2020-10-14 17:02:08 -0700

    finished crud API

--- database/seedDB.js
+++ database/seedDB.js
@@ -28,23 +28,23 @@ for (var i = 0; i < 100; i++ ) {
   locations.push(db.createLocation(location));
 }
 
-var trips = []
-for (var i = 0; i <= 100; i++ ) {
-  var numberOfResevervations = Math.floor(Math.random() * 25);
-  for (var j = 0; j < numberOfResevervations; j++) {
-    var trip = {};
-    trip.locationId = i;
-    trip.checkIn = faker.date.soon(90);
-    trip.checkOut = faker.date.future(0.12, trip.checkIn);
-    trip.adults = Math.ceil(Math.random() * 10);
-    trip.children = Math.floor(Math.random() * 10);
-    trip.rooms = Math.ceil(Math.random() * trip.adults)
-    trips.push(db.save(trip))
-  }
-}
+// var trips = []
+// for (var i = 0; i <= 100; i++ ) {
+//   var numberOfResevervations = Math.floor(Math.random() * 25);
+//   for (var j = 0; j < numberOfResevervations; j++) {
+//     var trip = {};
+//     trip.locationId = i;
+//     trip.checkIn = faker.date.soon(90);
+//     trip.checkOut = faker.date.future(0.12, trip.checkIn);
+//     trip.adults = Math.ceil(Math.random() * 10);
+//     trip.children = Math.floor(Math.random() * 10);
+//     trip.rooms = Math.ceil(Math.random() * trip.adults)
+//     trips.push(db.save(trip))
+//   }
+// }
 
 Promise.all(locations)
-  .then(Promise.all(trips))
+  // .then(Promise.all(trips))
   .then(result => {
     mongoose.connection.close();
   });
